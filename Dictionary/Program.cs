edbf6b8c05a76a55a4d3f15b7fbe2edde1299494
cihan büyükkara <buyukkara.cihan@hotmail.com>
2021-01-23 18:05:52 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1,"Cihan");
+            myDictionary.Add(2,"Özge");
+            myDictionary.Add(3,"Sinan");
+            myDictionary.Add(4,"Yağmur");
+            myDictionary.Add(5,"Ensar");
+
+            Console.WriteLine("Count:"+myDictionary.Count);
+
+            foreach (var values in myDictionary.Values)
+            {
+                Console.WriteLine("values:" + values);
+            }
+        }
+    }
+}
